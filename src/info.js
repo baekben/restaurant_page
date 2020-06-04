caf551169db4883c340f7ea2bb225ebca2c97924
Ben <ben10baek@hotmail.com>
2020-06-03 19:12:21 -0700

    Initial commit

---
+++ src/info.js
@@ -0,0 +1,15 @@
+function nav() {
+  var content = document.getElementById('content');
+
+  var heading = document.createElement('h1');
+  var motto = document.createElement('h2');
+
+  heading.innerHTML = 'Some Fancy Restaurant Est.19XX';
+  motto.innerHTML =
+    'Bringing classic "insert European country here" food since 19XX';
+
+  content.appendChild(heading);
+  content.appendChild(motto);
+}
+
+export { nav };
